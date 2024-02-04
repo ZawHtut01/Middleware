b8c5dc0ade4d41001a8284f5a1f2718fb489356a
Zaw Htut Aung <zawhtut0010zha@gmail.com>
2024-02-04 23:33:34 +0630

    add name

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -21,6 +21,7 @@ namespace MiddlwareTest.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
+            var name = "Zaw Htut";
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
